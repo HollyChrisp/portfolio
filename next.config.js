0cd8c4e2768120fb038539f5c76c8938cd8aecb1
csmyth12 <conorsmyth12@gmail.com>
2025-06-09 20:19:52 +0100

    another attempted fix for portfolio download

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
-
-const isDev = phase === PHASE_DEVELOPMENT_SERVER
-
 const nextConfig = {
     output: "export",
     images: { unoptimized: true },
-    assetPrefix: isDev ? undefined : "/portfolio"
 }
 
 module.exports = nextConfig
