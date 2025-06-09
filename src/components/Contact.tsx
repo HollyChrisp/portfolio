d847f4b24cd554f0f2bfd73a04b186b65c11c7c4
csmyth12 <conorsmyth12@gmail.com>
2025-06-09 19:24:10 +0100

    bigger portfolio

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,7 +12,7 @@ export const Contact = () => (
                 <p className='text-lg my-2'>Phone: <Link href='tel:00447398 040560'>+44 7398 040560</Link></p>
                 <div className='flex flex-row items-center justify-center my-2'>
                     <p className='text-lg mr-2'>Checkout my portfolio: </p>
-                    <a href='/HollyChrisp_GD_Portfolio_Compressed.pdf' download='HollyChrispPortfolio_Compressed.pdf' className='text-indigo-700 hover:text-indigo-950'>
+                    <a href='/HollyChrisp_GD_Portfolio.pdf' download='HollyChrispPortfolio_Compressed.pdf' className='text-indigo-700 hover:text-indigo-950'>
                         <ArrowDownOnSquareIcon />
                     </a>
                 </div>
